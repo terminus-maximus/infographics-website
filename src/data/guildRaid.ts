4031bac171f1511da5479043098dea366818febc
Dan Foster <danfoster01@gmail.com>
2026-09-09 10:54:40 -0700

    Current Season = 109

--- src/data/guildRaid.ts
+++ src/data/guildRaid.ts
@@ -1,4 +1,4 @@
-export const currentGuildRaidSeasonNumber = "108";
+export const currentGuildRaidSeasonNumber = "109";
 
 export const currentGuildRaidSeason = {
   seasonNumber: currentGuildRaidSeasonNumber,
